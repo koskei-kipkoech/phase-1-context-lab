d4074860d547d403c7444f107317219d443e67e5
Patrick Kipkoech <patrickwayy@gmail.com>
2024-10-10 09:35:51 +0300

    Updates

--- index.js
+++ index.js
@@ -1,4 +1,55 @@
 /* Your Code Here */
+function createEmployeeRecord([firstName, familyName, title, payPerHour]){
+    return{
+        firstName: firstName,
+        familyName: familyName,
+        title: title,
+        payPerHour: payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+}
+
+let employeeData = [
+    ['moe', 'sizlak', 'barkeep', 2],
+    ['bartholomew', 'simpson', 'scamp', 3]
+]
+function createEmployeeRecords(employeeData){
+    return employeeData.map(createEmployeeRecord)
+}
+
+function createTimeInEvent(dateTimeString){
+    let [date, hour] = dateTimeString.split(' ');
+    let newEvent ={
+        type: 'TimeIn',
+        date:date,
+        hour: parseInt(hour,10)
+    }
+    this.timeInEvents.push(newEvent);
+    return this;
+}
+
+function createTimeOutEvent(dateTimeString){
+    let [date, hour] = dateTimeString.split(' ');
+    let newEvent ={
+        type: 'TimeOut',
+        date:date,
+        hour: parseInt(hour,10)
+    }
+    this.timeOutEvents.push(newEvent);
+    return this;
+}
+
+function hoursWorkedOnDate(date){
+    let timeIn = this.timeInEvents.find(event => event.date === date);
+    let timeOut = this.timeOutEvents.find(event => event.date === date)
+    return (timeOut.hour - timeIn.hour)/100;
+}
+
+function wagesEarnedOnDate(date){
+    let hours = hoursWorkedOnDate.call(this,date);
+    return hours * this.payPerHour
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -20,4 +71,12 @@ const allWagesFor = function () {
 
     return payable
 }
+function findEmployeeByFirstName(collection, firstNameString){
+    return collection.find(employee => employee.firstName === firstNameString)
+}
 
+function calculatePayroll(employeeRecords){
+    return employeeRecords.reduce((total,employee) => {
+        return total + allWagesFor.call(employee)
+    },0)
+}
